0bdcfb1a6f4c00d22197cfa28d798e6e7c5ed7dd
junhaha1 <wnsgk919@naver.com>
2025-03-31 00:13:22 +0900

    userEntity 필드 추가 및 초기화 메소드 추가

--- src/main/java/com/adela/dto/AddCommnetRequest.java
+++ src/main/java/com/adela/dto/AddCommnetRequest.java
@@ -2,6 +2,7 @@ package com.adela.dto;
 
 import com.adela.domain.Article;
 import com.adela.domain.BoardComment;
+import com.adela.domain.UserEntity;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 public class AddCommnetRequest {
     private String comment;
     private Article article;
+    private UserEntity userEntity;
     private String codeComment;
     private LocalDate regDate;
     private LocalDate updateDate;
@@ -22,11 +24,15 @@ public class AddCommnetRequest {
     public void connectionArticle(Article article){
         this.article = article;
     }
+    public void connectionUserEntity(UserEntity userEntity){
+        this.userEntity = userEntity;
+    }
 
     public BoardComment toEntity(){
         return BoardComment.builder()
                 .comment(comment)
                 .article(article)
+                .userEntity(userEntity)
                 .codeComment(codeComment)
                 .regDate(LocalDate.now())
                 .updateDate(null)
