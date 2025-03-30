587eb5337df2c820449d20a3b887d83cdf5705ea
junhaha1 <wnsgk919@naver.com>
2025-03-31 00:15:23 +0900

    commentRemove 메소드에서 articleId로 되어있던 오타 commentId로 수정

--- src/main/java/com/adela/controller/CommentGoodApiController.java
+++ src/main/java/com/adela/controller/CommentGoodApiController.java
@@ -1,11 +1,9 @@
 package com.adela.controller;
 
 import com.adela.dto.AddCommentGoodRequest;
-import com.adela.dto.AddGoodRequest;
 import com.adela.service.CommentGoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("board/comment")
@@ -20,9 +18,9 @@ public class CommentGoodApiController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/removegood/{articleId}/{userId}")
-    public ResponseEntity<String> commentRemove(@PathVariable Long articleId, @PathVariable String userId){
-        commentGoodService.commentRemove(articleId, userId);
+    @DeleteMapping("/removegood/{commentId}/{userId}")
+    public ResponseEntity<String> commentRemove(@PathVariable Long commentId, @PathVariable String userId){
+        commentGoodService.commentRemove(commentId, userId);
         return ResponseEntity.ok().build();
     }
 }
