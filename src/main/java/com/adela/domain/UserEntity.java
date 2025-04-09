6c44153bfdf053171c1d096222a9dfeffb9ace2e
junhaha1 <wnsgk919@naver.com>
2025-04-09 23:30:55 +0900

    Spring Security에서 사용하기 위해 UserDetails 상속하여 오버라이딩

--- src/main/java/com/adela/domain/UserEntity.java
+++ src/main/java/com/adela/domain/UserEntity.java
@@ -8,14 +8,19 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "userentity")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     //email
     @Id
@@ -45,5 +50,40 @@ public class UserEntity {
     public void update(String name, String pwd){
         this.name = name;
         this.pwd = pwd;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return pwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
