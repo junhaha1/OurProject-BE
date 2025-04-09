2029994eb162982777de126444d15b8de8d59979
junhaha1 <wnsgk919@naver.com>
2025-04-09 23:32:20 +0900

    불필요한 @EnableWebMvc 제거

--- src/main/java/com/adela/config/WebConfiguration.java
+++ src/main/java/com/adela/config/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.adela.configuration;
+package com.adela.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
