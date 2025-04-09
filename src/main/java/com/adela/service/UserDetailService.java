0a4cd577f69f32e3b920f594d516cafb484dc6a9
junhaha1 <wnsgk919@naver.com>
2025-04-09 23:30:18 +0900

    토근을 발급하기 위한 사용자 조회 서비스

---
+++ src/main/java/com/adela/service/UserDetailService.java
@@ -0,0 +1,24 @@
+package com.adela.service;
+
+import com.adela.domain.UserEntity;
+import com.adela.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+//토큰 발급하기 위한 사용자 조회
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserEntity loadUserByUsername(String userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(()->new IllegalArgumentException((userId)));
+    }
+}
